2df4dcbad00785646e4b0469623c907c28c939a5
Fabian Barreto Sanchez <macfabian@MacBook-Air-de-Fabian.local>
2020-03-27 20:45:04 -0500

    se empieza a ajustar las señales de myo sin funcionamiento

--- GUI_Adquisicion/GUI_adquisicion_funcional.py
+++ GUI_Adquisicion/GUI_adquisicion_funcional.py
@@ -26,12 +26,15 @@ import wx.gizmos as gizmos
 from tkinter import *
 import sys
 import matplotlib.animation as manim
+import threading
 
 
 import subprocess
 import os
 from MYO_conexion import *
 import signal
+
+import myo
 
 
 ###########################################################################
@@ -530,7 +533,6 @@ class FrameGesto1 (wx.Frame):
                           pos=wx.DefaultPosition, size=wx.Size(1400, 800), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
 
         self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
-
         bSizer4 = wx.BoxSizer(wx.VERTICAL)
         self.panel = wx.Panel(self)
         sizer = wx.BoxSizer(wx.VERTICAL)
@@ -683,6 +685,13 @@ class FrameGesto1 (wx.Frame):
         self.m_button32.Bind(wx.EVT_BUTTON, self.OnClickInicio)
         self.button_siguiente.Bind(wx.EVT_BUTTON, self.OnClickConcentimiento)
         self.button_salir.Bind(wx.EVT_BUTTON, self.OnClickSalir)
+        # Arrancar conexion myo
+        # self.mainMYO()
+        
+        def prueba():
+            self.mainMYO()
+        hilo1 = threading.Thread(target=prueba)
+        hilo1.start()
 
     def __del__(self):
         pass
@@ -756,5 +765,77 @@ class FrameGesto1 (wx.Frame):
         t = str(m) + ":" + str(s)
         self.led.SetValue(t)
         self.OnTimer(None, c)
-
     
+    def mainMYO(self):
+        print("main")
+        myo.init()
+        hub = myo.Hub()
+        listener = EmgCollector(512)
+        time.sleep(5)
+        Plot(listener).main()
+    
+    
+
+class EmgCollector(myo.DeviceListener):
+  """
+  Collects EMG data in a queue with *n* maximum number of elements.
+  """
+
+  def __init__(self, n):
+    self.n = n
+    self.lock = Lock()
+    self.emg_data_queue = deque(maxlen=n)
+    print(self.n)
+
+  def get_emg_data(self):
+    with self.lock:
+      return list(self.emg_data_queue)
+
+  # myo.DeviceListener
+
+  def on_connected(self, event):
+    event.device.stream_emg(True)
+
+  def on_emg(self, event):
+    with self.lock:
+      self.emg_data_queue.append((event.timestamp, event.emg))
+
+class Plot(object):
+
+  def __init__(self, listener):
+    print("init plot")
+    self.n = listener.n
+    self.listener = listener
+    self.fig = plt.figure()
+    self.axes = [self.fig.add_subplot('81' + str(i)) for i in range(1, 9)]
+    [(ax.set_ylim([-100, 100])) for ax in self.axes]
+    self.graphs = [ax.plot(np.arange(self.n), np.zeros(self.n))[0] for ax in self.axes]
+    plt.ion()
+
+  def update_plot(self):
+    time.sleep(5)
+    print("Update")
+    emg_data = self.listener.get_emg_data()
+    print("data puro")
+    print(emg_data)
+    emg_data = np.array([x[1] for x in emg_data]).T
+    print("Secont data")
+    print(emg_data)
+    # for separa los 8 canales del emg
+    for g, data in zip(self.graphs, emg_data):
+      if len(data) < self.n:
+        # Fill the left side with zeroes.
+        data = np.concatenate([np.zeros(self.n - len(data)), data])
+        print("Dato if")
+        print(data)
+      g.set_ydata(data)
+      print("Dato canal")
+      print(g)
+      print(data)
+    plt.draw()
+
+  def main(self):
+    print("maii update")
+    while True:
+    #   self.update_plot()
+      plt.pause(1.0 / 30)
--- GUI_Adquisicion/pruebas.py
+++ GUI_Adquisicion/pruebas.py
@@ -1,32 +1,17 @@
-import wx
-class Frame(wx.Frame):
-    def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(-1, -1))
-        self.panel = wx.Panel(self)
-        self.Bind(wx.EVT_CLOSE, self.OnCloseWindow)
-        self.btn = wx.Button(self.panel, -1, "Name-a-matic")
-        self.Bind(wx.EVT_BUTTON, self.GetName, self.btn)
-        self.txt = wx.TextCtrl(self.panel, -1, size=(140,-1))
-        self.txt.SetValue('name goes here')
+import threading
 
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.btn)
-        sizer.Add(self.txt)
+def contar():
+    '''Contar hasta cien'''
+    contador = 0
+    while contador<100:
+        contador+=1
+        print('Hilo:', 
+              threading.current_thread().getName(), 
+              'con identificador:', 
+              threading.current_thread().ident,
+              'Contador:', contador)
 
-        self.panel.SetSizer(sizer)
-        self.Show()
-
-    def GetName(self, e):
-
-        dlg = wx.TextEntryDialog(self.panel, 'Whats yo name?:',"name-o-rama","", 
-                style=wx.OK)
-        dlg.ShowModal()
-        self.txt.SetValue(dlg.GetValue())
-        dlg.Destroy()
-
-    def OnCloseWindow(self, e):
-        self.Destroy()
-
-app = wx.App()
-frame = Frame(None, 'My Nameomatic')
-app.MainLoop()
+hilo1 = threading.Thread(target=contar)
+hilo2 = threading.Thread(target=contar)
+hilo1.start()
+hilo2.start()
